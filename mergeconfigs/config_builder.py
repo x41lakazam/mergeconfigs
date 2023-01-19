9bf2b2072e9ba1b253a3a2455ff77b60f91535d5
x41lakzaam <x41lakazam@gmail.com>
2023-01-19 22:31:22 +0200

    Add common keys security

--- mergeconfigs/config_builder.py
+++ mergeconfigs/config_builder.py
@@ -5,29 +5,29 @@ import yaml
 from yaml import Loader
 
 
-#VAR_REGEX = r"\${([^|]+?)}"
 VAR_REGEX = r"\${(\w+@[.\w]+)}"
 
 
-def dic_update(d, u):
+def _dic_update(d, u):
     for k, v in u.items():
         if isinstance(v, collections.abc.Mapping):
-            d[k] = dic_update(d.get(k, {}), v)
+            d[k] = _dic_update(d.get(k, {}), v)
         else:
             d[k] = v
     return d
 
-def _get_var(var_name, parent_vars):
+
+def _get_var(var_name, ctx):
     """var_name is in format file@variable"""
     try:
         in_file, var = var_name.split("@")
     except ValueError:
         raise Exception(f"Bad variable name {var_name}")
 
-    if in_file not in parent_vars:
-        raise Exception(f"No such file {in_file}: {list(parent_vars.keys())}")
+    if in_file not in ctx:
+        raise Exception(f"No such file {in_file}: {list(ctx.keys())}")
 
-    v = parent_vars.get(in_file)
+    v = ctx.get(in_file)
     subkeys = var.split(".")
     while subkeys:
         k = subkeys.pop(0)
@@ -38,17 +38,17 @@ def _get_var(var_name, parent_vars):
 
     return v
 
-def _resolve_variables(val, content):
+def _resolve_variables(val, ctx):
     if isinstance(val, list):
-        return [_resolve_variables(e, content) for e in val]
+        return [_resolve_variables(e, ctx) for e in val]
     elif isinstance(val, dict):
-        return {k:_resolve_variables(v, content) for k,v in val.items()}
+        return {k:_resolve_variables(v, ctx) for k,v in val.items()}
 
     if not isinstance(val, str):
         return val
 
     try:
-        val = re.sub(VAR_REGEX, lambda m: str(_get_var(m.groups()[0], content)), val)
+        val = re.sub(VAR_REGEX, lambda m: str(_get_var(m.groups()[0], ctx)), val)
     except Exception as exc:
         print(f"Error on value {val}: {exc}")
         raise
@@ -56,14 +56,14 @@ def _resolve_variables(val, content):
     return val
 
 
-def _resolve_yaml(file: Path, workdir=Path("."), env=".", parent_vars=None, callers=None):
+def _resolve_yaml(file: Path, workdir=Path("."), env=".", ctx=None, callers=None):
     callers = callers or []
-    parent_vars = parent_vars or {}
+    ctx = ctx or {}
 
     file_content = yaml.load(open(file.absolute()), Loader=Loader) or {}
 
     file_vars = file_content.copy()
-    parent_vars[file.stem] = file_vars
+    ctx[file.stem] = file_vars
 
     lines = open(file.absolute()).readlines()
 
@@ -72,28 +72,32 @@ def _resolve_yaml(file: Path, workdir=Path("."), env=".", parent_vars=None, call
         if line.startswith("#extends"):
             extends_yaml = line.split(" ")[1].strip()
             fpath = workdir.joinpath(extends_yaml)
-            content = _resolve_yaml(fpath, workdir, env, parent_vars, callers+[file.stem])
-            dic_update(content, file_content)
-            file_content = content
+            extends_content = _resolve_yaml(fpath, workdir, env, ctx, callers + [file.stem])
+            _dic_update(extends_content, file_content)
+            file_content = extends_content
 
         elif line.startswith("#include"):
             include_yaml = line.split(" ")[1].strip()
             fpath = workdir.joinpath(include_yaml)
             if fpath.stem in callers:
                 raise RecursionError(f"Circular import of {fpath.name} in {file.name}")
-            content = _resolve_yaml(fpath, workdir, env, parent_vars, callers+[file.stem])
-            dic_update(content, file_content)
+            include_content = _resolve_yaml(fpath, workdir, env, ctx, callers + [file.stem])
+            common_keys = include_content.keys() & file_content.keys()
+            if common_keys:
+                print(f"WARNING: Included file {include_yaml} have common keys with parent file, overwriting: {common_keys=}")
+
+            file_content.update(include_content)
 
         elif line.startswith("#load"):
             load_yaml = line.split(" ")[1].strip()
             fpath = workdir.joinpath(load_yaml)
             if fpath.stem in callers:
                 raise RecursionError(f"Circular import of {fpath.name} in {file.name}")
-            content = _resolve_yaml(fpath, workdir, env, parent_vars, callers+[file.stem])
-            parent_vars[fpath.stem] = content
+            content = _resolve_yaml(fpath, workdir, env, ctx, callers + [file.stem])
+            ctx[fpath.stem] = content
 
     for k,v in file_content.items():
-        file_content[k] = _resolve_variables(v, parent_vars)
+        file_content[k] = _resolve_variables(v, ctx)
 
     return file_content
 
